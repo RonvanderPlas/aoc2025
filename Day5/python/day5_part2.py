98d29a74bd587742a57866b3cb08166ea206cea6
Ron van der Plas <r.vanderplas@homewizard.com>
2025-12-05 08:55:45 +0100

    Day5 part2

--- Day5/python/day5_part2.py
+++ Day5/python/day5_part2.py
@@ -1,11 +1,30 @@
 input_file="../input/input.txt"   
 
 def count_invalid_inputs(ranges, inputs):
-    valid_ids = set()
-    for low,high in ranges:
-        for i in range(low, high + 1):
-            valid_ids.add(i)
-    return len(valid_ids)
+    sorted_ranges = sorted(ranges, key=lambda x: x[0])
+    filtered_ranges = []
+    total_count = 0
+
+    current_low, current_high = sorted_ranges[0]
+    for new_low, new_high in sorted_ranges[1:]:
+        #if the new low is within the current range, extend the current range if needed
+        if new_low <= current_high and new_high >= current_high:
+            current_high = new_high
+            #print(f"Extended range to: {current_low}-{current_high}")
+        
+        #else, if the new low is outside the current range, save the current range and start a new one
+        elif new_low > current_high:
+            filtered_ranges.append((current_low, current_high))
+            current_low, current_high = new_low, new_high
+            #print(f"New range started: {current_low}-{current_high}")
+    
+    #append the last range
+    filtered_ranges.append((current_low, current_high))
+    
+    for low, high in filtered_ranges:
+        diff = high - low + 1
+        total_count += diff
+    return total_count
 
 def main():
     ranges = []
